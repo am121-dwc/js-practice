b6b221ff5db06ab015120f3bf5b24e8b9c159ed2
am121-dwc <aoimorisugi121@gmail.com>
2024-04-17 15:57:28 +0000

    save

---
+++ furijs/chap1-9-1.js
@@ -0,0 +1,73 @@
+// let moji = prompt('Your age ?');
+// if ( ! isNaN(moji)){
+//   let age = parseInt(moji)
+//   if ( age >= 6 && age <= 15 ){
+//     console.log('義務教育')
+//   }else if ( age < 20 ){
+//     console.log('not adult')
+//   }else if( age < 65){
+//     console.log('adult')
+//   }else{
+//     console.log('senior')
+//   }
+
+// }
+
+
+// for( let cnt = 0; cnt < 10; cnt++ ){
+//   console.log( cnt + ' times loop')
+// }
+
+// for(let cnt = 10; cnt > 0; cnt-- ){
+//   console.log( cnt + ' last')
+// }
+
+
+// for(let x = 1; x < 10; x++ ){
+//   for(let y = 1; y < 10; y++ ){
+//     console.log( x + 'x' + y + '=' + x * y);
+//   }
+// }
+
+// let wdays = ['月','火','水','木','金'];
+// for( let cnt = 0; cnt < 5; cnt++ ){
+//   console.log( wdays[cnt] + '曜日');
+// }
+
+// let team = ['A','B','C','D','E'];
+// for ( let t1 of team){
+//   for( let t2 of team){
+//     if( t1 == t2 ){
+//       continue
+//     }else{
+//       console.log( t1 + 'vs' + t2);
+//     }
+//   }
+// }
+
+// let team = ['A','B','C','D','E'];
+// for ( let t1 of team){
+//   for( let t2 of team){
+//     if( t1 == t2 || t1 > t2 ){
+//       continue
+//     }else{
+//       console.log( t1 + 'vs' + t2);
+//     }
+//   }
+// }
+
+// let dir = ['east', 'west', 'south', 'north'];
+// for (let i = 0; i < dir.length; i++){
+//   let d = dir[i];
+//   console.log(d);
+// }
+
+// let wdays = ['月','火','水','木','金'];
+// for( let cnt = 4; cnt >= 0; cnt-- ){
+//   console.log( wdays[cnt] + '曜日');
+// }
+
+
+// for( let c of '月火水木金'){
+//   console.log(c);
+// }
